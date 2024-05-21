0044dc9f2c8118554579145ac715c332051a3428
Solara <solara.at@proton.me>
2024-05-21 20:14:52 +0200

    first commit

---
+++ readability.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* prompt the user for a string input
+* count the number of letters, words and sentences and store those values
+* compute them using the index 
+* output the grade level as an int value */
+
+int main(void)
+{
+// coleman-liau index: index = 0.0588 * L - 0.296 * S - 15.8
+// L = letters/100 words S = Sentences/100words
+}

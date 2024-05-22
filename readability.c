5b2b4a09bb103ea9bf50d4afca96aaf15e9df551
Solara <solara.at@proton.me>
2024-05-22 20:42:30 +0200

    definitions and increment loop

--- readability.c
+++ readability.c
@@ -1,12 +1,46 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /* prompt the user for a string input
 * count the number of letters, words and sentences and store those values
 * compute them using the index 
 * output the grade level as an int value */
 
-int main(void)
+#define M 256
+
+int main(int argc, char* argv[])
 {
-// coleman-liau index: index = 0.0588 * L - 0.296 * S - 15.8
-// L = letters/100 words S = Sentences/100words
+  char *text = malloc(M);
+  if (text == "NULL")
+  {
+    printf("no memory\n");
+    return 1;
+  }
+  printf("Text: ");
+  fgets(text, M, stdin);
+  printf("%s", text);
+
+  int letters, words, sentences = 0;
+
+  for (int i = 0, n = strlen(text); i < n; i++)
+  {
+    if (text[i] >= 'a' && text[i] <= 'z' || text[i] >= 'A' && text[i] <= 'Z')
+    {
+      letters += 1;
+    }
+    else if (text[i] == ' ')
+    {
+      words += 1;
+    }
+    else if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+    {
+      sentences += 1;
+    }
+  }
+  // n° of words = n° of whitespaces + 1
+  words += 1;
+
+  printf("%i %i %i\n", letters, words, sentences);
 }
+

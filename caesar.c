56201906b3cbfc8de6b0bfa7b3f4b1bc395d2b4e
Solara <solara.at@proton.me>
2024-06-21 20:01:29 +0200

    first commit

---
+++ caesar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* if nothing is typed at stdin, print a help message and return an error
+* if user types a positive int value, prompt them for plaintext
+* use the key to encrypt it and output cyphertext */
+
+#define M 512
+
+int main(int argc, char* argv[])
+{
+  if (argv[1] == NULL)
+  {
+    printf("Usage: ./caesar key\n");
+    return 1;
+  }
+}

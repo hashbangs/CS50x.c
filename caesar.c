cb2894b206ce0a6798cd45621a5e110caff2d9ac
Solara <solara.at@proton.me>
2024-06-22 12:10:33 +0200

    add tocypher function

--- caesar.c
+++ caesar.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* if nothing is typed at stdin, print a help message and return an error
 * if user types a positive int value, prompt them for plaintext
 * use the key to encrypt it and output cyphertext */
 
 #define M 512
+
+char* tocypher(char x[M]);
 
 int main(int argc, char* argv[])
 {
@@ -18,5 +21,15 @@ int main(int argc, char* argv[])
   printf("plaintext: ");
   fgets(plain, M, stdin);
 
-  printf("%s\n", plain);
+  char* cypher = tocypher(plain);
+  printf("%s\n", cypher);
+}
+
+char* tocypher(char x[M])
+{
+  for (int i = 0, n = strlen(x); i < n; i++)
+    {
+      x[i] += 1;
+    }
+  return x;
 }
